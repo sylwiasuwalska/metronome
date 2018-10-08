88097bed13c99d64ba5c377107ce31d323a4b0f3
Sylwia Suwalska <sylwia.suwalska@gmail.com>
2018-10-08 23:38:30 +0200

    first version of metronome

---
+++ script.js
@@ -0,0 +1,73 @@
+'use strict';
+
+class Metronome extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			bpm: 100,
+			label: "Start"
+		}
+		this.running = false;
+	}
+
+	bpmChange(event) {
+		const bpm = event.target.value;
+		this.setState({bpm})
+	}
+
+	startStop(event) {
+		event.preventDefault();
+		if (!this.running) {
+			this.running = true;
+			this.play();
+			this.setState({label: "Stop"});
+		} else {
+			this.running = false;
+			this.setState({label: "Start"});
+		}
+	}
+
+	play() {
+		var self = this;
+
+		return new Promise(
+			function(resolve, reject) {
+				self.playSound();	
+
+				const mili = 60/self.state.bpm * 1000;
+				setTimeout(
+					function() {
+						if (self.running) {
+							resolve();
+						}
+					}, 
+					mili
+				);
+			}
+		).then(this.play.bind(this));
+	}
+
+	playSound() {
+		var context = new AudioContext()
+		var o = context.createOscillator()
+		var g = context.createGain()
+		o.connect(g)
+		g.connect(context.destination)
+
+		o.start(0)
+		g.gain.exponentialRampToValueAtTime(0.00001, context.currentTime + 0.01)
+	}
+
+	render() {
+		return 	<div>
+					<form>
+						<p>{this.state.bpm} BPM</p>
+						<input type="range" className="form-control-range" min="30" max="300" value={this.state.bpm} onChange={this.bpmChange.bind(this)} />
+						<button onClick={this.startStop.bind(this)}>{this.state.label}</button>
+					</form>
+
+				</div>
+	}
+}
+
+ReactDOM.render(<Metronome />, document.getElementById('metronome'));
