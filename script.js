f414aa67831594c5a18d7cbd75b8912ea98ce2c9
Sylwia Suwalska <sylwia.suwalska@gmail.com>
2020-03-26 18:31:47 +0100

    syntax formatted

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-'use strict';
+"use strict";
 
-var context = new AudioContext()
-
+var context = new AudioContext();
 
 class Metronome extends React.Component {
 	constructor(props) {
@@ -9,13 +8,13 @@ class Metronome extends React.Component {
 		this.state = {
 			bpm: 100,
 			label: "Start"
-		}
+		};
 		this.running = false;
 	}
 
 	bpmChange(event) {
 		const bpm = event.target.value;
-		this.setState({bpm})
+		this.setState({ bpm });
 	}
 
 	startStop(event) {
@@ -23,56 +22,67 @@ class Metronome extends React.Component {
 		if (!this.running) {
 			this.running = true;
 			this.play();
-			this.setState({label: "Stop"});
+			this.setState({ label: "Stop" });
 		} else {
 			this.running = false;
-			this.setState({label: "Start"});
+			this.setState({ label: "Start" });
 		}
 	}
 
 	play() {
 		var self = this;
 
-		return new Promise(
-			function(resolve, reject) {
-				self.playSound();	
+		return new Promise(function(resolve, reject) {
+			self.playSound();
 
-				const mili = 60/self.state.bpm * 1000;
-				setTimeout(
-					function() {
-						if (self.running) {
-							resolve();
-						}
-					}, 
-					mili
-				);
-			}
-		).then(this.play.bind(this));
+			const mili = (60 / self.state.bpm) * 1000;
+			setTimeout(function() {
+				if (self.running) {
+					resolve();
+				}
+			}, mili);
+		}).then(this.play.bind(this));
 	}
 
 	playSound() {
-		var o = context.createOscillator()
+		var o = context.createOscillator();
 		o.frequency.value = 200;
-		var g = context.createGain()
-		o.connect(g)
-		g.connect(context.destination)
-		o.start(context.currentTime)
-		g.gain.exponentialRampToValueAtTime(0.00001, context.currentTime + 0.01)
+		var g = context.createGain();
+		o.connect(g);
+		g.connect(context.destination);
+		o.start(context.currentTime);
+		g.gain.exponentialRampToValueAtTime(
+			0.00001,
+			context.currentTime + 0.01
+		);
 	}
 
 	render() {
-		return 	<div className="row unmargin container-fluid align-items-center">
-					<div className="col">
-						<img src="https://crosseyedpianist.files.wordpress.com/2011/09/ayqq3hncmaaqvkh.jpg" />
-					</div>
-					<form className="col">
-						<p>{this.state.bpm} BPM</p>
-						<input type="range" className="form-control-range input-spacer" min="30" max="300" value={this.state.bpm} onChange={this.bpmChange.bind(this)} />
-						<button className="btn btn-outline-dark"onClick={this.startStop.bind(this)}>{this.state.label}</button>
-					</form>
-
+		return (
+			<div className="row unmargin container-fluid align-items-center">
+				<div className="col">
+					<img src="https://crosseyedpianist.files.wordpress.com/2011/09/ayqq3hncmaaqvkh.jpg" />
 				</div>
+				<form className="col">
+					<p>{this.state.bpm} BPM</p>
+					<input
+						type="range"
+						className="form-control-range input-spacer"
+						min="30"
+						max="300"
+						value={this.state.bpm}
+						onChange={this.bpmChange.bind(this)}
+					/>
+					<button
+						className="btn btn-outline-dark"
+						onClick={this.startStop.bind(this)}
+					>
+						{this.state.label}
+					</button>
+				</form>
+			</div>
+		);
 	}
 }
 
-ReactDOM.render(<Metronome />, document.getElementById('metronome'));
+ReactDOM.render(<Metronome />, document.getElementById("metronome"));
